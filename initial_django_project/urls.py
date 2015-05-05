d70f7dbece2581a4057c70f1a5e25d6b760a34a0
Andrew Akers <andrewrdakers@gmail.com>
2015-05-04 20:50:24 -0700

    did way more stuff than I should, but it's a tutorial, I'm not really worried about it at the moment...

--- initial_django_project/urls.py
+++ initial_django_project/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     # url(r'^$', 'initial_django_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
